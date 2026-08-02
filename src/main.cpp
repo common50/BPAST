6d91f584730bb9f73d87c4f958cd0e89a94fb0e7
common50 <mittenscanmeow@gmail.com>
2026-08-02 18:57:18 +0200

    added le triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
-// graphics engine but i lowkey cheat using opengl and glfw cus im lazy and dont want to write my own graphics engine from scratch
+// graphics engine but i lowkey cheat using opengl and glfw cus im lazy 
 // be patient and sit still
+
+// 2 lines for functions else ill go insane
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+
 // clutter comment slop just for you because i have nothing better to do and some ppl like it
 
-int main() {
+// verticies n stuff
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f  
+};
+
+// shader source strings or wtv its called im still not sure how it works
+// vertex shader:
+const char* vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+}
+)";
+
+// fragment shader:
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
+
+
+// make window and context and stuff
+GLFWwindow* makeMyWindowsComeTrue() {
     if (!glfwInit()) {
         std::cerr << "yo glfwInit failed haha good luck" << std::endl; // cus ppl always get angry at me if i dont do err handling
-        return -1; // should i make this 0 haha
+        return nullptr; // should i make this 0 haha
     }
 
-    // lowkey ripped these from wiki hahaha
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -23,22 +54,73 @@ int main() {
     if (!window) {
         std::cerr << "yo the window isnt working gang helppppp" << std::endl;
         glfwTerminate();
-        return -1;
+        return nullptr;
     }
 
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "gladLoadGLLoader failed, do better next time i dont hav all day" << std::endl;
-        return 1;
+        return nullptr;
     }
 
-    // versioj check cus ppl tend to mess version stuff up and i alwaus need to fix it for them
-    std::cout << "current version " << glGetString(GL_VERSION) << std::endl;
+    return window;
+}
 
+
+// create shader program and compile shaders and whatnot
+unsigned int createMeowProgram() {
+    // vertex part
+    unsigned int meowtexShader = glCreateShader(GL_VERTEX_SHADER); // meowwwww
+    glShaderSource(meowtexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(meowtexShader);
+
+    // fragment part
+    unsigned int meowmentShader = glCreateShader(GL_FRAGMENT_SHADER); // meowwwww
+    glShaderSource(meowmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(meowmentShader);
+
+    // program part
+    unsigned int meowProgram = glCreateProgram(); // meowwwww
+    glAttachShader(meowProgram, meowtexShader);
+    glAttachShader(meowProgram, meowmentShader);
+    glLinkProgram(meowProgram);
+
+    // cleanup part
+    glDeleteShader(meowtexShader);
+    glDeleteShader(meowmentShader);
+    // there HAS to be someone out there that got stuck cus they deleted the program aswell
+
+    return meowProgram;
+}
+
+
+// mess with gpu buffers and vertex arrays
+void GPUseless(unsigned int& VAO, unsigned int& VBO) {
+    // VAO n VBO part
+    // was gonna name em sum stupid like meowVAO n meowVBO 
+    // but maybe not they seem too important to be named like that
+    glGenVertexArrays(1, &VAO); // 1 vao pls
+    glGenBuffers(1, &VBO); // 1 vbo pls
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+
+// main loop for rendering and stuff
+void loopsoup(GLFWwindow* window, unsigned int shaderProgram, unsigned int VAO) {
     while (!glfwWindowShouldClose(window)) { // who named this dawg
         glClearColor(0.15f, 0.15f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -46,7 +128,35 @@ int main() {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
     }
+}
 
-    glfwTerminate(); //mem leaksskskskks
+
+// "free my boy ram he aint do nun"
+void cleanupcrew(unsigned int VAO, unsigned int VBO, unsigned int shaderProgram, GLFWwindow* window) {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+}
+
+
+int main() {
+    GLFWwindow* window = makeMyWindowsComeTrue();
+    if (!window) return -1;
+
+    unsigned int shaderProgram = createMeowProgram();
+
+    unsigned int VAO, VBO;
+    GPUseless(VAO, VBO);
+
+    // versioj check cus ppl tend to mess version stuff up and i alwaus need to fix it for them
+    std::cout << "current version " << glGetString(GL_VERSION) << std::endl;
+
+    loopsoup(window, shaderProgram, VAO);
+
+    cleanupcrew(VAO, VBO, shaderProgram, window);
     return 0;
 }
+
+// k so vertex = points in 3d space n fragment = color
+// vao n vbo = gpu memory stuff
