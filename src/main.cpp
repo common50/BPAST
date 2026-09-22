4345a1b35cc4bf619faecb15bf03ef5cda76cc01
common50 <mittenscanmeow@gmail.com>
2026-09-22 23:35:18 +0200

    meowmera.hpp thing

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 // 2 lines for functions else ill go insane
 
+// git isnt committing properly so im putting this comment rq imma delete this
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include <BPAST/scene/SceneObj.hpp>
