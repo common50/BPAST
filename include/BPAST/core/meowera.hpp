24d8893d0fdc1508fed6b7014db036f7245b3b9c
common50 <mittenscanmeow@gmail.com>
2026-09-23 12:33:43 +0200

    working on camera but my class is ending so uhh i gotta clean up

--- include/BPAST/core/meowera.hpp
+++ include/BPAST/core/meowera.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cmath>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -11,11 +12,48 @@ public:
     float myaw;
     float meowch;
 
+
     meowmera() {
         paws = glm::vec3(0.0f, 0.0f, 0.0f);
         front = glm::vec3(0.0f, 0.0f, -1.0f);
         up = glm::vec3(0.0f, 1.0f, 0.0f);
         myaw = -90.0f;
         meowch = 0.0f;
+    }
+
+
+    glm::mat4 getViewMatrix() const {
+        return glm::lookAt(paws, paws + front, up);
+    }
+
+
+    void chasingMice(float meoffsetX, float meoffsetY, float nonsense= 0.1f) {
+        meoffsetX *= nonsense;
+        meoffsetY *= nonsense;
+
+        myaw += meoffsetX;
+        meowch += meoffsetY;
+
+        if(meowch > 89.0f)
+            meowch = 89.0f;
+        if(meowch < -89.0f)
+            meowch = -89.0f;
+
+        glm::vec3 muffins = glm::vec3(0.0f, 0.0f, 0.0f);
+        muffins.x = cos(glm::radians(myaw)) * cos(glm::radians(meowch));
+        muffins.y = sin(glm::radians(meowch));
+        muffins.z = sin(glm::radians(myaw)) * cos(glm::radians(meowch));
+
+        front = glm::normalize(muffins);
+    }
+
+    // fyi prsd = pressed cus i dont want long var names they tire out my poor fingers
+    void meowermaMove(float Wprsd, float Sprsd, float Aprsd, float Dprsd,
+        float Spaceprsd, float Ctrlprsd, float Shiftprsd, float deltaTime) {
+
+        float fritters = 2.5f * deltaTime; // speed
+        if(Shiftprsd) fritters *= 2.5f;
+
+
     }
 }
